ed5147604a0b40bdb73d88dee3d27d1ebf4951e9
Joo Lee <joo@endowus.com>
2020-04-05 16:43:01 +0800

    font added

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Quicksand',
+        primarySwatch: Colors.purple
+      ),
       title: 'Flutter App',
       home: MyHomePage(),
     );
